a3cc1ef14c1a311d3f357c13d6aadc917c00018e
pawel.musial <pawel.musial@PK6-09>
2023-07-15 15:53:05 +0200

    Dodaj pliki projektów.

---
+++ TestProject/TaskManagerTest.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplication;
+
+namespace TestProject
+{
+    internal class TaskManagerTest
+    {
+        private TaskManager _taskManager;
+        [SetUp]
+        public void Setup()
+        {
+            _taskManager = new TaskManager();
+        }
+        [Test]
+        public void AddTask_ShouldIncreaseTaskCount()
+        {
+            // Arrange
+            var task = new WebApplication.Task("Test task");
+            // Act
+            _taskManager.AddTask(task);
+            // Assert
+            Assert.AreEqual(1, _taskManager.GetTasks().Count);
+        }
+
+
+        [Test]
+        //c
+        public void MarkTaskAsCompleted_ExistingTask_ShouldMarkTaskAsCompleted()
+        {
+            //Arrage
+            var task = new WebApplication.Task("Task c");
+            //Act
+            _taskManager.MarkTaskAsCompleted(20);
+            //Assert
+            Assert.IsTrue(!task.IsCompleted);
+        }
+
+        [Test]
+        //d
+        public void MarkTaskAsCompleted_NonExistingTask_ShouldNotMarkTaskAsCompleted()
+        {
+            //Arrage
+            var task = new WebApplication.Task("Task d");
+            //Act
+            _taskManager.MarkTaskAsCompleted(25);
+            //Assert
+            Assert.That(task.IsCompleted, Is.False);
+        }
+
+        [Test]
+        //a
+        public void RemoveTask_ExistingTask_ShouldDecreaseTaskCount()
+        {
+            //Arrage
+            var task = new WebApplication.Task("Task a");
+            //Act
+            _taskManager.RemoveTask(1);
+            //Assert
+            Assert.AreEqual(_taskManager.GetTasks().Count, );
+        }
+
+
+
+    }
+
+    
+
+}
+
---
+++ TestProject/UnitTest1.cs
@@ -0,0 +1,20 @@
+using WebApplication;
+
+namespace TestProject
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+
+    }
+}
